6cb60b43fa895157df873e0d2110e48c89bacc79
Orchard Du <orcharddu@gmail.com>
2024-03-12 03:56:06 +0800

    Impl Display trait for GolCell

--- src/util/cell.rs
+++ src/util/cell.rs
@@ -3,3 +3,9 @@ pub struct GolCell {
     pub x: usize,
     pub y: usize,
 }
+
+impl std::fmt::Display for GolCell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
